0dd2332397fc3e3294cba8727ae0df3216f6bfac
chu Oscar <chh179@ucsd.edu>
2019-06-01 23:12:06 -0700

    a lots

--- cls_site/text_classifier/urls.py
+++ cls_site/text_classifier/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.classifier, name='classifier'),
+    path('part1', views.part1, name='part1'),
+    path('part2', views.part2, name='part2'),
 ]
+
--- cls_site/text_classifier/views.py
+++ cls_site/text_classifier/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django import forms
+
 from .cls import TC
 
 class SentenceForm(forms.Form):
     test_sentence = forms.CharField(label='Test sentence', max_length=100)
 
-tc_sentiment = TC(0)
+#tc_sentiment = TC(0)
 
 def classifier(request):
+    form = SentenceForm()
+    # redirect from main page
+    if request.method == 'REDIRECT':
+        return HttpResponse("hah")
+    '''
     global tc_sentiment
     score = tc_sentiment.classify("This food is so bad that i cant even smell")
     form = 0
@@ -22,3 +28,12 @@ def classifier(request):
     else:
         form = SentenceForm()
     return render(request, 'text_classifier/classifier.html', {'explanation':'Enter your test sentence','form':form, 'score': 0})
+    '''
+    return render(request, 'text_classifier/page.html', {})
+
+def part1(request):
+    form = SentenceForm()
+    return render(request, 'text_classifier/part1.html', {'form':form})
+
+def part2(request):
+    return HttpResponse("todo")
